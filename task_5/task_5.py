7085481c27c834eb99511b7fd98badcea81b2dfe
Aleksandr <go280286sai@gmail.com>
2021-09-04 17:39:05 +0300

    correct with flake8

--- task_5/task_5.py
+++ task_5/task_5.py
@@ -1,31 +1,36 @@
-my_list=['один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять', 'десять']
-n=int(input())
-def my_fun(a,b):
-    if len(a)/n>=1:
-        k=len(a)//b
-        l=len(a)%b
-        if l>0 and l<=k:
-            l=l+1
-        elif l>0:
-            l=l+1
-        mas=[]
-        total=''
-        m=0
-        s=k
-        while k<=len(a)-l:
+my_list = ['один', 'два', 'три', 'четыре', 'пять',
+           'шесть', 'семь', 'восемь', 'девять', 'десять']
+n = int(input())
+
+
+def my_fun(a, b):
+    if len(a) / n >= 1:
+        k = len(a) // b
+        lq = len(a) % b
+        if lq > 0 and lq <= k:
+            lq = lq + 1
+        elif lq > 0:
+            lq = lq + 1
+        mas = []
+        total = ''
+        m = 0
+        s = k
+        while k <= len(a) - lq:
             for i in range(m, k):
-                total+=a[i]+' '
-            total=total.rstrip()
+                total += a[i] + ' '
+            total = total.rstrip()
             mas.append(total)
-            m=m+s
-            k+=s
-            total=''
-        if l>0:
+            m = m + s
+            k += s
+            total = ''
+        if lq > 0:
             for i in range(m, len(a)):
-                total+=a[i]+' '
-            total=total.rstrip()
-            mas.append(total)            
+                total += a[i] + ' '
+            total = total.rstrip()
+            mas.append(total)
         return mas
     else:
         return 0
-print(my_fun(my_list,n))
+
+
+print(my_fun(my_list, n))
